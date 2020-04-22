0942274ca91dd0f92c425197eb19437a27b0d022
chocobe <nice_10bird@naver.com>
2020-04-22 17:56:02 +0900

    <master> : Created "Tip3"

---
+++ Tip2_jQuery기능을 Vue로 통합하기/date-picker/src/DatePicker.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    달력: <input id="calendar">
+  </div>
+</template>
+
+
+<script>
+export default {
+  mounted() {
+    $("#calendar").datepicker();
+    $("#calendar").datepicker("option", "showAnim", "bounce");
+  }
+}
+</script>
